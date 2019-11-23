47f67838d86e5621c2bba803ea8244eba4806da7
William Koller <wkoller25@gmail.com>
2019-11-23 14:55:18 -0300

    grafico em linha

--- primeiroGrafico.php
+++ primeiroGrafico.php
@@ -1,6 +1,5 @@
 <html>
 <head>
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript">
         google.charts.load('current', {'packages':['corechart']});
         google.charts.setOnLoadCallback(drawChart);
@@ -27,7 +26,7 @@
             var options = {
                 title: 'População das Cidades',
                 curveType: 'function',
-                legend: { position: 'right' }
+                legend: { position: 'top' }
             };
 
             var chart = new google.visualization.LineChart(document.getElementById('grafico_linha'));
@@ -37,6 +36,6 @@
     </script>
 </head>
 <body>
-<div id="grafico_linha" style="width: 900px; height: 500px"></div>
+<div id="grafico_linha" style="height: 400px; width: 1200px"></div>
 </body>
 </html>
