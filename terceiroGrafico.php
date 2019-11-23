fafb92a30de37c29b8b217bd9bfaf7807e7593b2
William Koller <wkoller25@gmail.com>
2019-11-23 14:55:45 -0300

    grafico pizza

---
+++ terceiroGrafico.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+    <script type="text/javascript">
+        google.charts.load('current', {'packages':['corechart']});
+        google.charts.setOnLoadCallback(drawChart);
+
+        function drawChart() {
+            var data = google.visualization.arrayToDataTable([
+                ['Cidade', 'População'],
+                <?php
+                include 'conexao.php';
+                $sql = 'SELECT * FROM cidades';
+                $buscar = mysqli_query($conexao, $sql);
+
+                while($data = mysqli_fetch_array($buscar)){
+                $cidade = $data['cidade'];
+                $populacao = $data['populacao'];
+
+
+                ?>
+                ['<?php echo $cidade?> ',  <?php echo $populacao?> ],
+
+                <?php } ?>
+            ]);
+
+            var options = {
+                title: 'População de Cidades'
+            };
+
+            var chart = new google.visualization.PieChart(document.getElementById('grafico_pizza'));
+
+            chart.draw(data, options);
+        }
+    </script>
+</head>
+<body>
+<div id="grafico_pizza" style="height: 400px; width: 400px;"></div>
+</body>
+</html>
