7b7a049aea4736adaabae3ee1067d534333b5035
William Koller <wkoller25@gmail.com>
2019-11-23 12:58:39 -0300

    adding first graphic Google Charts

---
+++ primeiroGrafico.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+        google.charts.load('current', {'packages':['corechart']});
+        google.charts.setOnLoadCallback(drawChart);
+
+        function drawChart() {
+            var data = google.visualization.arrayToDataTable([
+                ['Cidade', 'População',  { role: 'annotation' }],
+                <?php
+                    include 'conexao.php';
+                    $sql = 'SELECT * FROM cidades';
+                    $buscar = mysqli_query($conexao, $sql);
+
+                    while($data = mysqli_fetch_array($buscar)){
+                        $cidade = $data['cidade'];
+                        $populacao = $data['populacao'];
+
+
+                ?>
+                ['<?php echo $cidade?> ',  <?php echo $populacao?>, <?php echo $populacao?> ],
+
+                <?php } ?>
+            ]);
+
+            var options = {
+                title: 'População das Cidades',
+                curveType: 'function',
+                legend: { position: 'right' }
+            };
+
+            var chart = new google.visualization.LineChart(document.getElementById('grafico_linha'));
+
+            chart.draw(data, options);
+        }
+    </script>
+</head>
+<body>
+<div id="grafico_linha" style="width: 900px; height: 500px"></div>
+</body>
+</html>
