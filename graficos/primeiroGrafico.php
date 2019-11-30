017a047da7cb9e46da75fe70c57dce1042e6cadd
William Koller <wkoller25@gmail.com>
2019-11-30 00:57:22 -0300

    first graficos

--- graficos/primeiroGrafico.php
+++ graficos/primeiroGrafico.php
@@ -1,5 +1,7 @@
 <html>
 <head>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+
     <script type="text/javascript">
         google.charts.load('current', {'packages':['corechart']});
         google.charts.setOnLoadCallback(drawChart);
@@ -8,7 +10,7 @@
             var data = google.visualization.arrayToDataTable([
                 ['Cidade', 'População',  { role: 'annotation' }],
                 <?php
-                    include 'conexao.php';
+                    include 'conexao/conexao.php';
                     $sql = 'SELECT * FROM cidades';
                     $buscar = mysqli_query($conexao, $sql);
 
