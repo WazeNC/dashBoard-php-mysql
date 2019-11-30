90ddf2ecb2836e5786c739db0e242bf11bc97c35
William Koller <wkoller25@gmail.com>
2019-11-30 01:01:50 -0300

    insert charts.js

---
+++ painel2.php
@@ -0,0 +1,84 @@
+<html>
+<head>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+        google.charts.load('current', {'packages':['corechart']});
+        google.charts.setOnLoadCallback(drawChart);
+
+        function drawChart() {
+            var data = google.visualization.arrayToDataTable([
+                ['Mes', 'Quantidade'],
+
+                <?php
+                include "conexao/conexao.php";
+                $sql = "SELECT * FROM clientes";
+                $buscar = mysqli_query($conexao, $sql);
+
+                while($dados = mysqli_fetch_array($buscar)){
+                   $mes = $dados['mes'];
+                   $quantidade = $dados['quantidade'];
+                ?>
+                ['<?php echo $mes?>', <?php echo $quantidade?>],
+               <?php } ?>
+            ]);
+
+            var options = {
+                title: '',
+                // curveType: 'function',
+                legend: { position: 'bottom' }
+            };
+
+            var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+            chart.draw(data, options);
+        }
+    </script>
+    <script type="text/javascript">
+        google.charts.load('current', {'packages':['corechart']});
+        google.charts.setOnLoadCallback(drawChart);
+
+        function drawChart() {
+
+            var data = google.visualization.arrayToDataTable([
+                ['Vendas', 'Quantidade'],
+                <?php
+                include "conexao/conexao.php";
+                $sql = "SELECT * FROM vendas";
+                $buscar = mysqli_query($conexao, $sql);
+
+                while($dados = mysqli_fetch_array($buscar)){
+                $mes = $dados['mes'];
+                $quantidade = $dados['quantidade'];
+                ?>
+
+                ['<?php echo $mes?>', <?php echo $quantidade?>],
+                <?php } ?>
+            ]);
+
+            var options = {
+                title: ''
+            };
+
+            var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+            chart.draw(data, options);
+        }
+    </script>
+</head>
+<body>
+
+<div class="container-fluid" style="margin-top: 5px;">
+    <div class="row">
+        <div class="col-md-8">
+            <h4>Gráfico de Clientes</h4>
+            <div id="curve_chart" ></div>
+        </div>
+        <div class="col-md-4">
+            <h4>Quantidade de Venda</h4>
+            <div id="piechart" style="width: 288px;"></div>
+        </div>
+    </div>
+
+</div>
+</body>
+</html>
