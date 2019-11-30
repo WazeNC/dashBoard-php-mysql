e2ed5cd5eed6ac8a6bc70797bea2d5be7b6817e1
William Koller <wkoller25@gmail.com>
2019-11-30 00:59:44 -0300

    insert cadastro de vendas

---
+++ cadastroVendas2.php
@@ -0,0 +1,13 @@
+<?php
+include "conexao/conexao.php";
+
+$mes = $_POST['mes'];
+$quantidade = $_POST['quantidade'];
+$valor = $_POST['valor'];
+
+$sql = "INSERT INTO vendas (mes, quantidade, valor)
+        VALUES ('$mes', $quantidade, $valor)";
+$inserir = mysqli_query($conexao, $sql);
+
+header('Location: dashboardTemplate.php?pagina=vendas');
+?>
