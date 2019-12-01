743f4504f285fe4f22dce38c43cbd9ceebf3ed63
William Koller <wkoller25@gmail.com>
2019-12-01 00:52:38 -0300

    adding sombra nos cards

--- painel.php
+++ painel.php
@@ -6,12 +6,19 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <style>
+        #sombra {
+            -webkit-box-shadow: 7px 9px 23px 0px rgba(191,189,191,1);
+            -moz-box-shadow: 7px 9px 23px 0px rgba(191,189,191,1);
+            box-shadow: 7px 9px 23px 0px rgba(191,189,191,1);
+        }
+    </style>
 </head>
-<body style="background-color: #f3fef3f3">
+<body>
 <div class="container-fluid">
     <div class="row">
         <div class="col-md-4">
-            <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
+            <div class="card text-white bg-primary mb-3" style="max-width: 18rem;" id="sombra">
                 <div class="card-header">Total Clientes / Ano</div>
                 <div class="card-body">
                     <h5 class="card-title" style="text-align: center; font-size: 40px;">
@@ -29,7 +36,7 @@
             </div>
         </div>
         <div class="col-md-4">
-            <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+            <div class="card text-white bg-success mb-3" style="max-width: 18rem;" id="sombra">
                 <div class="card-header">Faturamento / Ano</div>
                 <div class="card-body">
                     <h5 class="card-title" style="text-align: center; font-size: 40px;">
@@ -47,7 +54,7 @@
             </div>
         </div>
         <div class="col-md-4">
-            <div class="card text-white bg-danger mb-3" style="max-width: 18rem;">
+            <div class="card text-white bg-danger mb-3" style="max-width: 18rem;" id="sombra">
                 <div class="card-header">Total Vendas</div>
                 <div class="card-body">
                     <h5 class="card-title" style="text-align: center; font-size: 40px;">
