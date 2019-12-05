d12b7d2139e7b72e0a7fe666dc3ada05617eb513
William Koller <wkoller25@gmail.com>
2019-12-04 22:56:15 -0300

    alter index to dashboard

--- index.php
+++ index.php
@@ -149,7 +149,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 <script src="https://kit.fontawesome.com/de5a2d2c68.js" crossorigin="anonymous"></script>
-<script type="text/javascript" src="gulpfile.js">
+<script type="text/javascript" src="gulpFile.js">
     $(window).resize(function () {
         drawChart();
         drawChart2();
