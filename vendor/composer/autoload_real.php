03a2d0b24b1adafcb1ea4059a4f668f4bc988c22
William Koller <wkoller25@gmail.com>
2019-12-05 00:07:21 -0300

    vendot/

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -47,6 +47,24 @@ class ComposerAutoloaderInitd22f889e5db4cdec629aa7671eb4e8ad
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInitd22f889e5db4cdec629aa7671eb4e8ad::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequired22f889e5db4cdec629aa7671eb4e8ad($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequired22f889e5db4cdec629aa7671eb4e8ad($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
